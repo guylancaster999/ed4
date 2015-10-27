2d42cb25a8bea97f36dd1e25c17814a382bf62ab
guy lancaster <guylancaster@hotmail.com>
2015-10-27 13:17:02 +0000

    commit2

--- mngr.php
+++ mngr.php
@@ -4,15 +4,12 @@ Page::logged_in();
 print Page::hdr("M");
 ?>
 <body>
-
 <div class="container">
- 
-  <div class="row">
+   <div class="row">
     <div class="col-sm-8">
 	<h3>Manage Data</h3>
          </div>
-   
-    <div class="col-sm-4">
+       <div class="col-sm-4">
        <?php Page::Mngr_Menu();?>
     </div>
   </div>
--- tmp2.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head></head>
-<body>
-<iframe src="http://www.youtube.com/embed/Sz7fmqgKZiM" width="640" height="400"></iframe>
-
-</body>
-</html>
